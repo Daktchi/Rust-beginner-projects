2610209b2e489303dfb3c6e8877d7232d72374a8
Daktchi <daktchiphilippe@gmail.com>
2026-08-09 18:29:35 +0100

    Remove root-level Rust files and folders

--- target/debug/deps/Dice_roller-d8f6d6bcc77a3b25.d
+++
@@ -1,5 +0,0 @@
-/storage/emulated/0/Dev_root/rust_projects/Dice_roller/target/debug/deps/Dice_roller-d8f6d6bcc77a3b25.d: src/main.rs
-
-/storage/emulated/0/Dev_root/rust_projects/Dice_roller/target/debug/deps/Dice_roller-d8f6d6bcc77a3b25: src/main.rs
-
-src/main.rs:
